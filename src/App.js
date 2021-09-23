acb22d44671897e30192b4a215e23ae7944cb2af
jnsmtnr <janosmaitner@gmail.com>
2021-09-23 09:07:48 +0200

    fix test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 function App() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Button>nini</Button>
+      <Button>this is material-ui</Button>
     </div>
   );
 }
