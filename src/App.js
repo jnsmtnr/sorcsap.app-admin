a0aa96514115f173e01570628085a9b90dd17cd0
jnsmtnr <janosmaitner@gmail.com>
2021-09-26 14:33:53 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react'
+
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Login from './components/Login'
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  function handleLogin(token) {
+    setToken(token)
+  }
+
   return (
     <Container maxWidth="md">
       <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <Login />
+        {!token && <Login onLogin={handleLogin} />}
+        {token && <div>Bejelentkezve</div>}
       </Box>
     </Container>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login form', () => {
   render(<App />);
-  const mui = screen.getByText(/this is material-ui/i);
+  const mui = screen.getByText(/Bejelentkezés az admin felületbe/i);
   expect(mui).toBeInTheDocument();
 });
