69ef44055eb63d08730e6accd08d99d1627ccdad
jnsmtnr <janosmaitner@gmail.com>
2021-09-27 09:06:22 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('react-redux', () => ({
+  useDispatch: jest.fn(),
+  useSelector: jest.fn()
+}))
+
 test('renders login form', () => {
   render(<App />);
   const mui = screen.getByText(/Bejelentkezés az admin felületbe/i);
