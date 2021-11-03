f25b05ea30b7304c112d0dd73fc6fd43232c6bad
jnsmtnr <janosmaitner@gmail.com>
2021-11-03 19:42:47 +0100

    add new beer

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -1,14 +1,26 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import api from '../api'
 
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import CircularProgress from '@mui/material/CircularProgress'
+
 import BeerList from '../components/Beers/BeerList'
+import BeerEditor from '../components/Beers/BeerEditor'
 
 export default function Beers() {
     const [beers, setBeers] = useState([])
+    const [open, setOpen] = useState(false)
 
-    useEffect(() => {
+    function openModal() {
+        setOpen(true)
+    }
+
+    function closeModal() {
+        setOpen(false)
+    }
+
+    const getBeers = useCallback(() => {
         api.get('/beers')
             .then(response => {
                 setBeers(response.data)
@@ -16,11 +28,23 @@ export default function Beers() {
             .catch(console.log)
     }, [])
 
+    useEffect(() => {
+        getBeers()
+    }, [getBeers])
+
     if (beers.length === 0) return (
         <Box sx={{ display: 'flex', height: 'calc(100vh - 48px)', justifyContent: 'center', alignItems: 'center' }}>
             <CircularProgress />
         </Box>
     )
 
-    return <BeerList beers={beers} />
+    return (
+        <React.Fragment>
+            <Box sx={{ textAlign: "center", marginTop: '1em' }}>
+                <Button onClick={openModal} variant="outlined">Új sör hozzáadása</Button>
+            </Box>
+            <BeerList beers={beers} />
+            {open && <BeerEditor open={open} onClose={closeModal} onSave={getBeers} />}
+        </React.Fragment>
+    )
 }
