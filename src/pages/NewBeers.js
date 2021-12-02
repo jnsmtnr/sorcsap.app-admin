d38e71cdd9b69285a91bbc957f2361995cc0ae40
jnsmtnr <janosmaitner@gmail.com>
2021-12-02 20:22:19 +0100

    handle no new beers

--- src/pages/NewBeers.js
+++ src/pages/NewBeers.js
@@ -9,24 +9,27 @@ import NewBeerList from '../components/NewBeers/NewBeerList.js'
 import api from '../api'
 
 export default function NewBeers() {
+    const [loading, setLoading] = useState(false)
     const [newBeers, setNewBeers] = useState([])
 
     useEffect(() => {
+        setLoading(true)
+
         api.get('/ratings/new-beers')
             .then((response) => setNewBeers(response.data))
             .catch(console.log)
+            .finally(() => setLoading(false))
     }, [])
-
-    if (newBeers.length === 0) return (
-        <Box sx={{ display: 'flex', height: 'calc(100vh - 48px - 1em)', justifyContent: 'center', alignItems: 'center' }}>
-            <CircularProgress />
-        </Box>
-    )
 
     return (
         <React.Fragment>
             <BackButton>Új Sörök</BackButton>
-            <NewBeerList beers={newBeers} />
+            {!loading && newBeers.length > 0 && <NewBeerList beers={newBeers} />}
+            {newBeers.length === 0 && (
+                <Box textAlign="center">
+                    {loading ? <CircularProgress /> : 'Nincs új sör'}
+                </Box>
+            )}
         </React.Fragment>
     )
 }
